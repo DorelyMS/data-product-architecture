3f6b877d6b6c5841ed082149051beef71789e8fc
DorelyMS <ubuntu@ip-172-31-22-129.us-west-2.compute.internal>
2021-05-25 02:40:40 +0000

    corre en ec2

--- src/utils/monitoreo.py
+++ src/utils/monitoreo.py
@@ -79,7 +79,7 @@ def fun_monit(predictions):
             figure=fig
         )
     ])
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=8050,debug=True)
 
 
 if __name__ == "__main__":
